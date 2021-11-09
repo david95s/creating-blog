260a7a9bc723e7d5f2fd03da5fa12aea3daeab43
David <davidlinconl1@hotmail.com>
2021-11-09 12:22:42 -0300

    Init page Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,8 @@
 import { GetStaticProps } from 'next';
+import Link from 'next/link';
+import { FiCalendar, FiUser } from 'react-icons/fi';
+
+import Header from '../components/Header';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,9 +28,54 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  /*
+  SpaceTravellingMyBlog
+  */
+  return (
+    <>
+      <main className={commonStyles.container}>
+        <Header />
+
+        <div className={styles.posts}>
+          <Link href="/">
+            <a>
+              <strong>Algum titulo</strong>
+              <p>Pensando em sincronização em vez e ciclos de vida.</p>
+              <ul>
+                <li>
+                  <FiCalendar />
+                  15 Mar 2021
+                </li>
+                <li>
+                  <FiUser />
+                  Gabriel Borges
+                </li>
+              </ul>
+            </a>
+          </Link>
+          <Link href="/">
+            <a className={styles.linkPost}>
+              <strong>Algum titulo</strong>
+              <p>Pensando em sincronização em vez e ciclos de vida.</p>
+              <ul>
+                <li>
+                  <FiCalendar />
+                  15 Mar 2021
+                </li>
+                <li>
+                  <FiUser />
+                  Gabriel Borges
+                </li>
+              </ul>
+            </a>
+          </Link>
+          <button type="button">Carregar mais posts</button>
+        </div>
+      </main>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
